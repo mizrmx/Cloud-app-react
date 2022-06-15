3d984d04ba43f2f3331dc8e2e314f0e2c87b728f
Mayra Zurita <mizrmx@gmail.com>
2022-06-15 10:14:35 -0500

    Update Hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Navbar from './components/Navbar'
-
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App () {
   return (
     <>
-     <Navbar></Navbar>
+      <Navbar />
+      <Hero />
     </>
   );
 }
