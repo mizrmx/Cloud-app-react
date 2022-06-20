6336e200fd4e90f2967c81e68e3b43abd957d50f
Mayra Zurita <mizrmx@gmail.com>
2022-06-20 08:43:46 -0500

    Update Pricing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Support from './components/Support';
 import AllInOne from './components/AllInOne';
+import Pricing from './components/Pricing';
 
 function App () {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Hero />z
       <About />
       <Support/>
       <AllInOne/>
+      <Pricing/>
     </>
   );
 }
